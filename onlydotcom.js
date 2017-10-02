23e030d00f69f2c5b5014dab21c0fd0aabbde994
ukasyahf462 <32339489+ukasyahf462@users.noreply.github.com>
2017-10-02 11:35:02 +0700

    Add files via upload

---
+++ onlydotcom.js
@@ -0,0 +1,1 @@
+var str= window.location.href.toString(); if ((str.indexOf('.com/'))=='-1') { var str1=str.substring(str.lastIndexOf(".blogspot.")); if (str1.indexOf('/')=='-1') { var str2=str1; } else { var str2=str1.substring(0,str1.indexOf('/')+1); } window.location.href =window.location.href.toString().replace(str2,'.blogspot.com/ncr/'); }
